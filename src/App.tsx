2757d3b256df995e246b209c752a47e54a05ca50
J. cob <jacoboosorio299589@gmail.com>
2026-02-04 15:08:45 -0500

    Creating the first component for the title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Title from "./components/Title";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        Hello world
+      <div className="min-h-screen bg-slate-800">
+        <Title />
       </div>
     </>
   )
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+export default function Title() {
+    return (
+        <>
+            <div>
+                <h1 className="text-center text-green-500">Pomodoro timer!</h1>
+            </div>
+        </>
+    );
+}
