3882469998a2dfadd83cd87ce68d724297270d2d
J. cob <jacoboosorio299589@gmail.com>
2026-02-11 17:25:01 -0500

    Adding colors and logic for the stop timer button for making it obvious to the user that the button is not clickable at the moment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 export default function Button(props: ButtonProps){
     return (
         <>
-            <button className="bg-green-400 text-green-100 rounded-lg p-2 mx-4"
+            <button className={`rounded-lg p-2 mx-4 ${props.disabled ? 'bg-gray-400 text-gray 100' : 'bg-green-400 text-green-100'}`}
             onClick={props.onClick}
             disabled={props.disabled}
             >
