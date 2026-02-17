eb6d26127d163d6b93c3dac81d1908e52d264e5f
J. cob <jacoboosorio299589@gmail.com>
2026-02-17 17:25:12 -0500

    Fixing timer logic with pomodoros counter, minutes changing and alerts

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import './App.css'
 import { useEffect, useState } from "react";
 
 function App() {
-  const minutes: number = 0.05 * 60;
+  const minutes: number = 0.1 * 60;
   const minutesBreak: number = 0.05 * 60;
-  let timerInitialTime = minutes;
+  //let timerInitialTime = minutes;
 
   const [timer, setTimer] = useState(minutes);
   const [isRunning, setIsRunning] = useState(false);
@@ -30,31 +30,42 @@ function App() {
   useEffect(() => {
       let interval: number | null = null;
 
-      if(isRunning && timer > 0){
-          interval = setInterval(() => {
-              setTimer((time) => time - 1);
-          }, 1000)
-      } else if(timer === 0 && timerInitialTime === minutes){
-        setMode("break");
-        setTimer(minutesBreak);
-        timerInitialTime = minutesBreak;
+      if(mode === "work"){
+        if(isRunning && timer > 0){
+            interval = setInterval(() => setTimer((time) => time - 1), 1000);
+        } else if(timer === 0){
+          if(pomodoros < 4){
+            setPomodoros(pomodoros + 1);
+          } else if(pomodoros === 4){
+            setPomodoros(0);
+            setIsRunning(false);
+          }
+  
+          setMode("break");
+          setTimer(minutesBreak);
+  
+          //Showing an alert to the user stating that the time for studying is over
+          alert("Time is up! Now It's time for you to rest");
+  
+          //timerInitialTime = minutesBreak;
+        }
+      } else{
+        if(isRunning && timer > 0){
+          interval = setInterval(() => setTimer((time) => time - 1), 1000);
+        } else if(timer === 0){
+          setMode("work");
+          setTimer(minutes);
 
-        if(pomodoros <= 4 && timerInitialTime === minutes){
-          setPomodoros(pomodoros + 1);
-        } else{
-          setPomodoros(0);
+          //Showing an alert to the user stating that the time for the break is over
+          alert("Break time is up! Now It's time to start working again");
         }
       }
 
-      //Showing an alert to the user stating that the time for studying is over
-      if(timer === 0){
-        alert("Time is up! Now It's time for you to rest");
-      }
 
       return () => {
           if(interval) clearInterval(interval);
       }
-  }, [isRunning, timer, mode, pomodoros, timerInitialTime]);
+  }, [isRunning, timer, mode, pomodoros, minutesBreak, minutes]);
   
   const formatTime = (seconds: number) => {
     const minutes: number = Math.floor((seconds / 60));
@@ -97,4 +108,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
